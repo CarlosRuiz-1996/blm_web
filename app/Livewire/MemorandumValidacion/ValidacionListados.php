903a8e461caf3b42501cfecb910af15acb3bf2c0
Carlos Ruiz <131738990+CarlosRuiz-1996@users.noreply.github.com>
2024-03-19 11:37:50 -0600

    validar memo

---
+++ app/Livewire/MemorandumValidacion/ValidacionListados.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\MemorandumValidacion;
+
+use Livewire\Component;
+
+class ValidacionListados extends Component
+{
+    
+    public function render()
+    {
+        return view('livewire.memorandum-validacion.validacion-listados');
+    }
+}
