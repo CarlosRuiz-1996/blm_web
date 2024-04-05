658cd16f7a2d3b554db9f8b8c8b61058fd531190
adrianqguerrero123 <adrian.quintanar@romicc.com.mx>
2024-04-05 11:18:51 -0600

    cambios en hr

--- app/Livewire/Rh/Altaempleado.php
+++ app/Livewire/Rh/Altaempleado.php
@@ -2,12 +2,145 @@
 
 namespace App\Livewire\Rh;
 
+use App\Models\Ctg_Area;
+use App\Models\Empleado;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Altaempleado extends Component
 {
+    public $data = [];
+    public $servicioId;
+    public $nombreServicio;
+    public $tipoServicio;
+    public $unidadMedida;
+    public $precioUnitario;
+    public $editarPrecio;
+    public $cantidad = 1;
+    public $isAdmin;
+    public $total;
+    public $estados;
+    public $municipios;
+    public $cp;
+    public $colonias;
+    public $cp_invalido;
+    public $ctg_cp_id = "";
+    public $puesto = "";
+    public $nombreContacto = "";
+    public $razonSocial;
+    public $rfc;
+    public $ctg_tipo_cliente_id;
+    public $tipoClientelist;
+    public $calleNumero;
+    public $telefono;
+    public $correoElectronico;
+    public $vigencia;
+    public $condicionpago;
+    public $condicionpagolist;
+    public $servicios;
+    public $precio_servicio;
+    public $totalreal;
+    public $valoridcoti;
+    public $valoriidser;
+    public $valoridcliente;
+    public $valoridusuario;
+    public $password;
+    public $apepaterno;
+    public $apematerno;
+    public $editarPreciocheck;
+    public $cantidadcheck;
+    public $cantidadhabilitado = false;
+    public $editarPreciohabilitado = false;
+    public $sexo;
+    public $areas;
+    public $area;
+    public $image;
+    public $fechaNacimiento;
+    
+   use WithFileUploads;
+
+
+    public function mount()
+    {
+        $this->colonias = collect();
+        $this->areas= Ctg_Area::all();
+    }
     public function render()
     {
         return view('livewire.rh.altaempleado');
+    }
+
+    public function validarCp()
+    {
+        $this->validate([
+            'cp' => 'required|digits_between:1,5',
+        ], [
+            'cp.digits_between' => 'El código postal solo contiene 5 digitos.',
+            'cp.required' => 'Código postal requerido.',
+
+        ]);
+        $codigo = DB::select("
+                SELECT DISTINCT cp.id, cp.colonia, m.municipio, e.name 
+                FROM ctg_cp cp 
+                LEFT JOIN ctg_estados e ON e.id = cp.ctg_estado_id
+                LEFT JOIN ctg_municipios m ON m.id = cp.ctg_municipio_id AND m.ctg_estado_id = e.id 
+                WHERE cp LIKE CONCAT('%', " . $this->cp . " , '%')
+            ");
+        if ($codigo) {
+            $this->municipios = $codigo[0]->municipio;
+            $this->estados = $codigo[0]->name;
+            $this->colonias = $codigo;
+            $this->cp_invalido = "";
+        } else {
+            $this->cp_invalido = "Codigo postal no valido";
+        }
+    }        
+    #[On('save-empleado')]
+    public function guaradaempleado(){
+            $this->validate([
+            'nombreContacto' => 'required|string|max:255',
+            'apepaterno' => 'required|string|max:255',
+            'apematerno' => 'nullable|string|max:255',
+            'puesto' => 'required|string|max:255',
+            'telefono' => 'required|string|max:20',
+            'correoElectronico' => 'required|email|max:255|unique:users,email',
+            'fechaNacimiento' => 'required|date',
+            'sexo' => 'required|in:Masculino,Femenino',
+            'area' => 'required',
+            'cp' => 'required|string|max:10',
+            'estados' => 'required|string|max:255',
+            'municipios' => 'required|string|max:255',
+            'ctg_cp_id' => 'required',
+            'calleNumero' => 'required|string|max:255',
+        ]);
+        try {
+            DB::transaction(function () {
+        $id= User::create([
+            'name' => $this->nombreContacto,
+            'paterno' => $this->apepaterno,
+            'materno' => $this->apematerno,
+            'email' => $this->correoElectronico,
+            'password' => bcrypt(123456789),
+        ]);
+        Empleado::create([
+            'user_id' => $id->id,
+            'direccion' => $this->calleNumero,
+            'ctg_cp_id' => $this->ctg_cp_id,
+            'sexo' => $this->sexo,
+            'phone' => $this->telefono,
+            'ctg_area_id' => $this->area,
+            'status_empleado' => 1,
+            'fecha_nacimiento' => $this->fechaNacimiento,
+        ]);
+    });
+        $this->dispatch('success', ['El empleado se creó con éxito']);
+    } catch (\Exception $e) {
+        dd($e->getMessage()); 
+        $this->dispatch('error', ['Ocurrió un error al registrar el empleado']);
+    }
+
     }
 }
--- resources/views/livewire/rh/altaempleado.blade.php
+++ resources/views/livewire/rh/altaempleado.blade.php
@@ -1,58 +1,198 @@
 <!-- resources/views/livewire/empleado-form.blade.php -->
 
 <div>
-    <form wire:submit.prevent="submitForm" enctype="multipart/form-data">
-        <div class="form-group">
-            <x-input-validado :readonly="true" label="RFC" placeholder="Ingrese el RFC" wire-model="rfc" wire-attribute="rfc" type="text" />
-        </div>
-        
-        <div class="form-group">
-            <x-input-validado label="Nombre" placeholder="Ingrese el nombre" wire-model="nombre" wire-attribute="nombre" type="text" />
-        </div>
-        
-        <div class="form-group">
-            <x-input-validado label="Apellido" placeholder="Ingrese el apellido" wire-model="apellido" wire-attribute="apellido" type="text" />
-        </div>
-        
-        <div class="form-group">
-            <x-input-validado label="Correo Electrónico" placeholder="Ingrese el correo electrónico" wire-model="email" wire-attribute="email" type="email" />
-        </div>
-        
-        <div class="form-group">
-            <x-input-validado label="Teléfono" placeholder="Ingrese el teléfono" wire-model="telefono" wire-attribute="telefono" type="tel" />
-        </div>
-        
-        <div class="form-group">
-            <label for="direccion">Dirección</label>
-            <textarea class="form-control" id="direccion" placeholder="Ingrese la dirección" wire:model="direccion"></textarea>
-        </div>
-        
-        <div class="form-group">
-            <label for="fecha_nacimiento">Fecha de Nacimiento</label>
-            <input type="date" class="form-control" id="fecha_nacimiento" wire:model="fecha_nacimiento">
-        </div>
-        
-        <div class="form-group">
-            <label for="fecha_inicio">Fecha de Inicio</label>
-            <input type="date" class="form-control" id="fecha_inicio" wire:model="fecha_inicio">
-        </div>
-        
-        <div class="form-group">
-            <label for="salario">Salario</label>
-            <input type="number" class="form-control" id="salario" placeholder="Ingrese el salario" wire:model="salario">
-        </div>
-        
-        <div class="form-group">
-            <label for="departamento">Departamento</label>
-            <input type="text" class="form-control" id="departamento" placeholder="Ingrese el departamento" wire:model="departamento">
-        </div>
-        
-        <div class="form-group">
-            <label for="foto">Foto</label>
-            <input type="file" class="form-control-file" id="foto" wire:model="foto" accept="image/*">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-outline card-info">
+                <div class="card-header">
+                    <h3 class="text-center">Empleado</h3>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12 mb-3">
+                            <div class="position-relative">
+                                <!-- Imagen cargada -->
+                                @if ($image)
+                                    <div class="text-center d-flex justify-content-center">
+                                        <img src="{{ asset($image->temporaryUrl()) }}" alt="Imagen cargada" width="200" height="200">
+                                    </div>
+                                @else
+                                    <!-- Imagen por defecto -->
+                                    <div class="text-center d-flex justify-content-center">
+                                        <img src="{{ asset('img/sinfoto.png') }}" alt="Imagen por defecto" width="200" height="200">
+                                    </div>
+                                @endif
+                                <!-- Icono del input -->
+                                <div class="position-absolute" style="top: 0; right: 35%;">
+                                    <label class="image" for="image-id"><i class="fa fa-lg fa-fw fa-plus"></i></label>
+                                    <input type="file" class="image" id="image-id" accept="image/*" hidden wire:model.live="image">
+                                </div>
+                            </div>
+                        </div>
+                                        
+                                                                    
+                        <div class="col-md-4 mb-3">
+                            <x-input-validado label="Nombre del contacto:"
+                                placeholder="Ingrese el Nombre del Contacto" wire-model="nombreContacto"
+                                wire-attribute="nombreContacto" type="text" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-input-validado label="Apellido Paterno:" placeholder="Ingrese el Apellido paterno"
+                                wire-model="apepaterno" wire-attribute="apepaterno" type="text" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-input-validado label="Apellido Materno:" placeholder="Ingrese el Apellido Materno"
+                                wire-model="apematerno" wire-attribute="apematerno" type="text" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-input-validado label="Puesto:" placeholder="Ingrese el Puesto" wire-model="puesto"
+                                wire-attribute="puesto" type="text" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-input-validado-telefono label="Telefono:" placeholder="Ingrese telefono"
+                                wire-model="telefono" wire-attribute="telefono" type="text" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-input-validado label="Correo Electrónico:" placeholder="Ingrese Correo Electronico"
+                                wire-model="correoElectronico" wire-attribute="correoElectronico" type="text" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-input-validado-date label="Fecha Nacimiento:" placeholder="Ingrese fecha de nacimiento"
+                                wire-model="fechaNacimiento" wire-attribute="fechaNacimiento" type="date" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-select-validado label="Sexo:" placeholder="Seleccione" wire-model="sexo"
+                            required>
+                                    <option value="Masculino">Masculino</option>
+                                    <option value="Femenino">Femenino</option>
+                        </x-select-validado>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <x-select-validado label="area:" placeholder="Seleccione" wire-model="area"
+                            required>
+                            @foreach ($areas as $area)
+                            <option value="{{ $area->id }}">{{ $area->name }}</option>
+                             @endforeach
+                        </x-select-validado>
+                        </div>
+
+
+                        
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card card-outline card-info">
+                                <div class="card-header">
+                                    <h3 class="text-center">Domicilio fiscal</h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-3 mb-3">
+                                            <x-input-validado label="Codigo Postal:" placeholder="Ingrese codigo postal"
+                                                wire-model="cp" wire-attribute="cp" type="text" />
+                                        </div>
+                                        <div class="col-md-3 mb-3 mt-2">
+                                            <div class="form-group ">
+                                                <label></label>
+                                                <button wire:click='validarCp' class="btn btn-secondary btn-block ">Validar
+                                                    cp</button>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 mb-3">
+                                            <x-input-validado label="Estado:" :readonly="true" placeholder="esperando..."
+                                                wire-model="estados" wire-attribute="estados" type="text" />
+                                        </div>
+                                        <div class="col-md-3 mb-3">
+                                            <div class="form-group">
+                                                <x-input-validado label="Alcaldia/Municipio:" :readonly="true"
+                                                    placeholder="esperando..." wire-model="municipios" wire-attribute="municipios"
+                                                    type="text" />
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-3">
+                                            <x-select-validado label="Colonia:" placeholder="Seleccione" wire-model="ctg_cp_id"
+                                                required>
+                                                @if (count($colonias))
+                                                    @foreach ($colonias as $cp)
+                                                        <option value="{{ $cp->id }}">{{ $cp->colonia }}</option>
+                                                    @endforeach
+                                                @else
+                                                    <option value="">Esperando...</option>
+                                                @endif
+                                            </x-select-validado>
             
+                                        </div>
+                                        <div class="col-md-6 mb-3">
+                                            <x-input-validado label="Calle y Número:" placeholder="Ingrese la Calle y Número"
+                                                wire-model="calleNumero" wire-attribute="calleNumero" type="text" />
+                                        </div>
+            
+            
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <button class="btn btn-info btn-block" wire:click="$dispatch('confirm')">Guardar</button>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
         </div>
-        
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
+    </div>
+    @push('js')
+    <script>
+        document.addEventListener('livewire:initialized', () => {
+
+            @this.on('confirm', () => {
+
+                Swal.fire({
+                    title: '¿Estas seguro?',
+                    text: "El usuario se Creara.",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Si, adelante!',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        @this.dispatch('save-empleado');
+                    }
+                })
+            })
+
+            Livewire.on('success', function([message]) {
+                console.log(message);
+
+                Swal.fire({
+                    icon: 'success',
+                    title: message[0],
+                    showConfirmButton: true,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        window.location.href = '/rh';
+
+                    }
+                });
+            });
+
+
+            Livewire.on('error', function([message]) {
+                Swal.fire({
+                    icon: 'error',
+                    title: message[0],
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+            });
+        });
+    </script>
+@endpush
 </div>
