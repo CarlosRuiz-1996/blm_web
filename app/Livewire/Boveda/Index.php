f4b85a397b08dfac684cc7edc6b8c0dc0e1d4a1a
Carlos Ruiz <131738990+CarlosRuiz-1996@users.noreply.github.com>
2024-09-26 16:23:30 -0600

    cambio en boveda, llaves, repro5

--- app/Livewire/Boveda/Index.php
+++ app/Livewire/Boveda/Index.php
@@ -494,22 +494,24 @@ class Index extends Component
 
     public function removeKey(ServicioKey $key)
     {
+        $keys = ServicioKey::where('ruta_servicio_id', $key->ruta_servicio_id)->count();
+        $rutaserv = RutaServicio::find($this->ruta_servicio->id);
         try {
             DB::beginTransaction();
            
 
-            $keys = ServicioKey::where('ruta_servicio_id', $key->ruta_servicio_id)->count();
-            $rutaserv = RutaServicio::find($this->ruta_servicio->id);
+          
             $rutaserv->keys = $keys > 0 ? 1 : 0;
             $rutaserv->save();
 
             $key->delete();
-            $this->llenarmodalservicios($rutaserv->ruta_id);
         } catch (Exception $e) {
             DB::rollBack();
             $this->dispatch('error', ['Hubo un error, intenta mas tarde.']);
         }
         $this->getKeys();
+        $this->llenarmodalservicios($rutaserv->ruta_id);
+
     }
     public function cleanKeys()
     {
