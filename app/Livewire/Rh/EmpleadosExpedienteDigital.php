5b02142579c0a5205e8f0ab3531d477541b511dc
adrianqguerrero123 <adrian.quintanar@romicc.com.mx>
2024-10-17 11:08:13 -0600

    cambios en rh perfil empleado

--- app/Livewire/Rh/EmpleadosExpedienteDigital.php
+++ app/Livewire/Rh/EmpleadosExpedienteDigital.php
@@ -5,14 +5,146 @@ namespace App\Livewire\Rh;
 use App\Models\CtgDocumentoExpedienteEmpleado;
 use App\Models\DocumentoEmpleado;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class EmpleadosExpedienteDigital extends Component
 {
-    public $empleadoId;
+    public $empleadoId;    
+    use WithFileUploads;
+
+    public $archivo; // Variable que guarda el archivo subido
+    public $tipoDocumentoId; // Variable que almacena el ID del tipo de documento
+    public $archivos = [];
+    // Reglas de validación para el archivo
+    protected $rules = [
+        'archivo' => 'required|mimes:jpg,jpeg,png,pdf|max:10240', // Limita tipos y tamaño del archivo (máximo 10 MB)
+    ];
+
+    public $isOpen = false;
+    public $isOpendos = false;
+    public $documentId;
+
+
+    
+
+    public function openModal($id)
+    {
+        $this->documentId = $id; // Asigna el ID al atributo
+        $this->isOpen = true; // Abre el modal
+    }
+
+    public function openModal2($id)
+    {
+        $this->isOpendos = true; // Abre el modal
+    }
+
+    
     public function render()
     {
         $tiposDocumentos = CtgDocumentoExpedienteEmpleado::all();
         $documentosPorEmpleado = DocumentoEmpleado::where('empleado_id',$this->empleadoId)->get();
         return view('livewire.rh.empleados-expediente-digital', compact('tiposDocumentos','documentosPorEmpleado'));
     }
+
+    // Subir archivos y guardar el registro en la base de datos
+    public function storeDocuments()
+    {
+        $this->validate([
+            'archivos.*' => 'file|max:2048', // Validar antes de almacenar
+            'empleadoId' => 'required|exists:empleados,id',
+            'documentId' => 'required|exists:ctg_documentos_expediente_empleados,id',
+        ]);
+
+        foreach ($this->archivos as $archivo) {
+            // Almacenar el archivo en storage/app/empleadosDoc
+            // Obtener el nombre del archivo original
+            // Obtener el nombre original sin la extensión
+            $filenamedos = pathinfo($archivo->getClientOriginalName(), PATHINFO_FILENAME);
+            // Limpiar el nombre del archivo, eliminando espacios y caracteres especiales
+            $cleanFilename = Str::slug($filenamedos, '_');
+
+            // Obtener la extensión del archivo
+            $extension = $archivo->getClientOriginalExtension();
+
+            // Buscar si existen otros archivos con el mismo nombre base
+            $existingFilesCount = DocumentoEmpleado::where('empleado_id', $this->empleadoId)
+                ->where('ctg_documentos_expediente_empleados_id', $this->documentId)
+                ->where('nombre_archivo', 'like', '%'.$cleanFilename.'.'.$extension . '%')
+                ->count();
+
+            // Crear el nombre del archivo con el contador
+            if ($existingFilesCount > 0) {
+                $filename = 'empleadoid_'.$this->empleadoId.'_documentoid_'.$this->documentId.'_'.$cleanFilename.'_' . ($existingFilesCount + 1) . '.' . $extension;
+            } else {
+                $filename = 'empleadoid_'.$this->empleadoId.'_documentoid_'.$this->documentId.'_'.$cleanFilename.'.' . $extension;
+            }
+                $path = $archivo->storeAs(path: 'empleadosDoc', name: $filename);
+
+            
+
+            // Crear el registro en la base de datos
+            DocumentoEmpleado::create([
+                'empleado_id' => $this->empleadoId,
+                'ctg_documentos_expediente_empleados_id' => $this->documentId,
+                'nombre_archivo' => $filename,
+                'url_archivo' => $path,  // Guardar la ruta donde se almacenó el archivo
+                'status_documento_empleado' => 1, // Puede ser 'pendiente', 'verificado', etc.
+            ]);
+        }
+
+        // Limpiar la lista de archivos después de guardarlos
+        $this->archivos = [];
+        $this->dispatch('clear-file-input');
+        $this->isOpen = false; // Abre el modal
+        session()->flash('message', 'Archivos subidos y almacenados correctamente.');
+    }
+    
+
+    public function removeFile($index)
+    {
+        if (isset($this->archivos[$index])) {
+            unset($this->archivos[$index]);
+            // Reindexar el array para evitar huecos
+            $this->archivos = array_values($this->archivos);
+        }
+    }
+
+    public function descargarDocumento($documentoId)
+{
+    // Buscar el documento en la base de datos
+    $documento = DocumentoEmpleado::find($documentoId);
+
+    if ($documento && Storage::exists($documento->url_archivo)) {
+        // Retornar el archivo para su descarga
+        return Storage::download($documento->url_archivo, $documento->nombre_archivo);
+    } else {
+        // Si no existe el archivo, mostrar un mensaje de error
+        session()->flash('error', 'El archivo no existe.');
+    }
+}
+public function eliminarDocumento($documentoId)
+{
+    // Buscar el documento en la base de datos
+    $documento = DocumentoEmpleado::find($documentoId);
+
+    if ($documento) {
+        // Eliminar el archivo del almacenamiento
+        if (Storage::exists($documento->url_archivo)) {
+            Storage::delete($documento->url_archivo);
+        }
+
+        // Eliminar el registro de la base de datos
+        $documento->delete();
+
+        // Mensaje de éxito
+        session()->flash('success', 'Documento eliminado exitosamente.');
+    } else {
+        // Si no se encuentra el documento, mostrar un mensaje de error
+        session()->flash('error', 'Documento no encontrado.');
+    }
+}
+
+
 }
--- resources/views/livewire/rh/empleados-expediente-digital.blade.php
+++ resources/views/livewire/rh/empleados-expediente-digital.blade.php
@@ -3,7 +3,7 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table">
-                    <thead class="table-info">
+                    <thead>
                         <tr>
                             <th>Nombre del Documento</th>
                             <th>Documentos Cargados</th>
@@ -12,28 +12,152 @@
                     </thead>
                     <tbody>
                         @foreach($tiposDocumentos as $tipoDocumento)
-                        <tr>
-                            <td>{{ $tipoDocumento->nombre }}</td>
-                            <td>
-                                @foreach($documentosPorEmpleado as $documento)
-                                @if($tipoDocumento->id == $documento->ctg_documentos_expediente_empleados_id)
-                                {{ $documento->nombre_archivo }}
-                                @endif
-                                @endforeach
-                            </td>
-                            <td>
-                                <!-- Acciones -->
-                                <button class="btn btn-sm btn-warning" href="#" title="Editar"><i
-                                        class="fas fa-edit"></i></button>
-                                <button class="btn btn-sm btn-danger" href="#" title="Eliminar"><i
-                                        class="fas fa-trash-alt"></i></button>
-                                <button class="btn btn-sm btn-info" href="#" title="Subir"><i class="fas fa-upload"></i></button>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>{{ $tipoDocumento->nombre }}</td>
+                                <td>
+                                    @php
+                                        // Verificar si hay documentos para el tipo de documento actual
+                                        $hasDocuments = $documentosPorEmpleado->contains('ctg_documentos_expediente_empleados_id', $tipoDocumento->id);
+                                    @endphp
+
+                                    @if ($hasDocuments)
+                                        <!-- Botón para abrir el collapse individual de cada tipo de documento -->
+                                        <button class="btn btn-sm btn-info" data-toggle="collapse" data-target="#collapseDocumentos{{ $tipoDocumento->id }}">
+                                            <i class="fas fa-folder-open"></i> Ver Documentos
+                                        </button>
+
+                                        <!-- Collapse individual para los documentos del tipo de documento -->
+                                        <div id="collapseDocumentos{{ $tipoDocumento->id }}" class="collapse mt-2">
+                                            <table class="table table-sm">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Nombre del Documento</th>
+                                                        <th>Acciones</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach($documentosPorEmpleado as $doc)
+                                                        @if($tipoDocumento->id == $doc->ctg_documentos_expediente_empleados_id)
+                                                            <tr>
+                                                                <td>{{ $doc->nombre_archivo }}</td>
+                                                                <td>
+                                                                    <button class="btn btn-sm btn-primary" wire:click="descargarDocumento({{ $doc->id }})">
+                                                                        <i class="fas fa-download"></i>
+                                                                    </button>
+                                                                    <button class="btn btn-sm btn-danger" wire:click="eliminarDocumento({{ $doc->id }})">
+                                                                        <i class="fas fa-trash-alt"></i>
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
+                                                        @endif
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    @else
+                                        <span>No hay documentos cargados</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    <button class="btn btn-sm btn-warning" wire:click="openModal({{ $tipoDocumento->id }})">
+                                        <i class="fas fa-upload"></i>
+                                    </button>
+                                </td>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+
+    <!-- Modal de Bootstrap -->
+    <div
+        x-data="{ show: @entangle('isOpen') }"
+        x-init="
+            $watch('show', value => {
+                if (value) {
+                    $('#myModal').modal('show');
+                } else {
+                    $('#myModal').modal('hide');
+                }
+            });
+            $('#myModal').on('hidden.bs.modal', () => {
+                if (show) {
+                    show = false;
+                }
+            });
+        "
+        id="myModal"
+        class="modal fade @if($isOpen) show @endif"
+        tabindex="-1"
+        aria-labelledby="myModalLabel"
+        aria-hidden="true"
+        wire:ignore.self
+        style="@if($isOpen) display: block; @endif"
+    >
+        <div class="modal-dialog modal-dialog-xl">
+            <div class="modal-content">
+                <div class="modal-header bg-info text-white text-center">
+                    <h5 class="modal-title" id="myModalLabel">Subir Documento</h5>
+                </div>
+                <div class="modal-body">
+                    <form wire:submit.prevent="uploadDocument">
+                        <div class="mb-3" wire:ignore>
+                            <label for="archivos" class="form-label">Seleccionar archivo(s)</label>
+                            <input type="file" class="form-control" id="archivos" wire:model="archivos" multiple>
+                        </div>
+
+                        @error('archivos.*') <span class="text-danger">{{ $message }}</span> @enderror
+
+                        <!-- Muestra los nombres de los archivos seleccionados con opción para eliminarlos -->
+                        <div>
+                            <h6>Archivos seleccionados:</h6>
+                            <ul class="list-group">
+                                @if ($archivos && count($archivos) > 0)
+                                    @foreach ($archivos as $index => $archivo)
+                                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                                            @php
+                                                // Obtener la extensión del archivo
+                                                $extension = strtolower($archivo->getClientOriginalExtension());
+                                            @endphp
+
+                                            @if (in_array($extension, ['jpg', 'jpeg', 'png', 'gif'])) <!-- Si es una imagen -->
+                                                <img src="{{ $archivo->temporaryUrl() }}" alt="{{ $archivo->getClientOriginalName() }}" style="width: 50px; height: 50px; object-fit: cover;" class="me-2">
+                                            @elseif ($extension === 'pdf') <!-- Si es un PDF -->
+                                                <span>{{ $archivo->getClientOriginalName() }} (PDF)</span>
+                                            @elseif (in_array($extension, ['doc', 'docx', 'xls', 'xlsx'])) <!-- Si es un documento de Office -->
+                                                <span>{{ $archivo->getClientOriginalName() }} (Documento)</span>
+                                            @else
+                                                <span>{{ $archivo->getClientOriginalName() }} (Tipo desconocido)</span>
+                                            @endif
+
+                                            <button type="button" class="btn btn-danger btn-sm" wire:click="removeFile({{ $index }})">Eliminar</button>
+                                        </li>
+                                    @endforeach
+                                @else
+                                    <li class="list-group-item">No se han seleccionado archivos.</li>
+                                @endif
+                            </ul>
+                        </div>
+
+                        <input type="hidden" wire:model="documentId">
+                    </form>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" wire:click="$set('isOpen', false)">Cerrar</button>
+                    <button type="submit" class="btn btn-primary" wire:click='storeDocuments'>Subir</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        document.addEventListener('livewire:initialized', function () {
+            Livewire.on('clear-file-input', () => {
+                document.getElementById('archivos').value = null;
+            });
+        });
+    </script>
 </div>
